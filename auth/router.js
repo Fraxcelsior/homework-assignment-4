f2166c412ed60fa58075fae97bde7c4d1576e0f5
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-29 21:54:19 +0200

    clean

--- auth/router.js
+++ auth/router.js
@@ -5,7 +5,6 @@ const User = require('../user/model')
 const auth = require('./middleware')
 const router = new Router()
 
-
 router.post('/tokens', function (req, res, next) {
     const emails = req.body.email
     const password = req.body.password
@@ -16,7 +15,6 @@ router.post('/tokens', function (req, res, next) {
         })
     }
     else {
-        //1. find user based on email, 'entity' is callback from promise
         User
             .findOne({
                 where: {
@@ -29,9 +27,7 @@ router.post('/tokens', function (req, res, next) {
                         message: 'E-mail or password was incorrect'
                     })
                 }
-                //2. use bcrypt.compareSync to check pw against stored hash
                 if (bcrypt.compareSync(req.body.password, entity.password)) {
-                    //3. if pw is correct, return JWT with userId of user (user.id)
                     res.send({
                         jwt: toJWT({ userId: entity.id })
                     })
@@ -48,14 +44,7 @@ router.post('/tokens', function (req, res, next) {
                     message: 'Oops! Something went wrong'
                 })
             })
-        //to implement steps 1-3, we used Sequelize and promise chaining^
     }
-})
-
-router.get('/secret-endpoint', auth, (req, res) => {
-    res.send({
-        message: `Thanks for visiting the secret endpoint, ${req.user.email}.`,
-    })
 })
 
 
--- song/router.js
+++ song/router.js
@@ -6,24 +6,6 @@ const Playlist = require('../playlist/model')
 
 const router = new Router()
 
-/*
-router.get('/player/', (req, res, next) => {
-    const limit = req.query.limit || 25
-    const offset = req.query.offset || 0
-
-    Promise.all([
-    Player
-        .count(),
-    Player
-        .findAll({
-            limit, offset
-        })])
-        .then(players => {
-            res.status(200).json({ players })
-        })
-        .catch(error => next(error))
-})
-*/
 router.get('/songs', (req, res, next) => {
     Song
         .findAll()
@@ -36,7 +18,6 @@ router.get('/songs', (req, res, next) => {
 router.post('/songs', auth, (req, res, next) => {
     const splitUrl = req.baseUrl.split("")
     const listId = splitUrl.slice(-1)[0]
-    
     Song
         .create( {
             title: req.body.title,
@@ -47,31 +28,5 @@ router.post('/songs', auth, (req, res, next) => {
         .then(song => { res.status(201).json(song) })
         .catch(error => next(error))
 })
-
-/*
-router.post('/song/', auth, (req, res, next) => {
-    const head = req.headers.authorization && req.headers.authorization.split(' ')
-    const data = toData(head[1])
-    Song
-        .create( {
-            name: req.body.name,
-            userId: data.userId
-        })
-        .then(playlist => { res.status(201).json(playlist) })
-        .catch(error => next(error))
-    
-})
-
-router.put('/player/:id', function (req, res, next) {
-    const id = req.params.id
-    Player
-        .findByPk(id)
-        .then(player => {
-            player.update(req.body)
-        })
-        .then(player => res.status(201).json(player))
-        .catch(error => next(error))
-})
-*/
 
 module.exports = router
